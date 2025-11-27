49e65c4e541b8ffab3ee5e16d716f091b5895c93
gkueny <gkueny.me@gmail.com>
2025-11-27 13:56:12 +0100

    setup repro

--- App.tsx
+++ App.tsx
@@ -1,37 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { StyleSheet, Text, View } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+export default function App() {
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text
+        style={styles.paragraph}
+        numberOfLines={1}
+        adjustsFontSizeToFit={true}
+      >
+        1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac
+        augue ut turpis rhoncus dignissim. Morbi sed nibh ut tellus tristique
+        pharetra in eget diam.
+      </Text>
+
+      <Text
+        style={[styles.paragraph, styles.paragraph2]}
+        numberOfLines={1}
+        adjustsFontSizeToFit={true}
+      >
+        2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac
+        augue ut turpis rhoncus dignissim. Morbi sed nibh ut tellus tristique
+        pharetra in eget diam.
+      </Text>
+      <Text
+        style={[styles.paragraph, styles.paragraph3]}
+        numberOfLines={1}
+        adjustsFontSizeToFit={true}
+      >
+        3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac
+        augue ut turpis rhoncus dignissim. Morbi sed nibh ut tellus tristique
+        pharetra in eget diam.
+      </Text>
+      <Text
+        style={[styles.paragraph, styles.paragraph4]}
+        numberOfLines={1}
+        adjustsFontSizeToFit={true}
+      >
+        4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac
+        augue ut turpis rhoncus dignissim. Morbi sed nibh ut tellus tristique
+        pharetra in eget diam.
+      </Text>
+      <Text
+        style={[styles.paragraph, styles.paragraph5]}
+        numberOfLines={1}
+        adjustsFontSizeToFit={true}
+      >
+        5. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac
+        augue ut turpis rhoncus dignissim. Morbi sed nibh ut tellus tristique
+        pharetra in eget diam.
+      </Text>
     </View>
   );
 }
@@ -39,7 +56,25 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+  },
+  paragraph: {
+    backgroundColor: 'blue',
+    padding: 2,
+    marginBottom: 10,
+    fontSize: 18,
+    color: 'white',
+  },
+  paragraph2: {
+    fontSize: 23,
+  },
+  paragraph3: {
+    fontSize: 25,
+  },
+  paragraph4: {
+    fontSize: 28,
+  },
+  paragraph5: {
+    fontSize: 30,
   },
 });
-
-export default App;
